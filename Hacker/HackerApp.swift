83dc5cf1d1e6def979cf45aa30197e6025d2c185
JakubDobDev <jakub.dobrzynski.dev@gmail.com>
2021-07-27 13:23:26 +0200

    Initial Commit

---
+++ Hacker/HackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerApp.swift
+//  Hacker
+//
+//  Created by mac on 27/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct HackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
